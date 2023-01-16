60e46772d541ae7adb61242510d17dc800cfe68b
UyenQuyen <uynwiin@gmail.com>
2023-01-16 23:02:45 +0700

    My Test

---
+++ main.js
@@ -0,0 +1,65 @@
+
+let IsPrime = (N) => {
+    let i = 2;
+    let flag = true;
+    if (N == 1 || N == 0)
+        flag = false;
+    else {
+        while (i <= Math.sqrt(N)) {
+            if (N % i == 0) {
+                flag = false;
+                break;
+            }
+            i++;
+        }
+    }
+    return flag;
+}
+
+function returnPrime() {
+    let a = parseInt(document.getElementById('first-number').value);
+    let b = parseInt(document.getElementById('second-number').value);
+    if (a > b || a == b || a < 0) {
+        let Notify = document.createElement('p');
+        Notify.textContent = "Error!!! a < b and a > 0"
+        document.getElementById('content').appendChild(Notify);
+        Notify.style.color = "red";
+        setTimeout(() => {
+            document.getElementById('content').removeChild(Notify);
+        }, 1000);
+    }
+    else {
+        let result = [];
+        for (let i = a; i <= b; i++) {
+            if (IsPrime(i)) {
+                result.push(i);
+            }
+        }
+        let Notify = document.createElement('p');
+        Notify.textContent = "The prime numbers between a and b are: " + result;
+        document.getElementById('content').appendChild(Notify);
+    }
+
+}
+
+
+function printTriangle() {
+    let height = document.getElementById('height-triangle').value;
+    let br = document.createElement('br');
+    height = parseInt(height);
+    for (i = 1; i <= height; i++) {
+        let line = [];
+        let makeLine = document.createElement('p');
+        for (j = 1; j <= i; j++) {
+            line += "* ";
+            makeLine.textContent = line;
+            document.getElementById('content').appendChild(makeLine);
+
+        }
+        document.getElementById('content').appendChild(br);
+    }
+
+
+}
+
+
